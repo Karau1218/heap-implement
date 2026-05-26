df03fc5263dde585912552c3f0e93a7691e3078d
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-05-26 12:19:29 -0700

    File skeletons

---
+++ src/Heap.java
@@ -0,0 +1,26 @@
+/**
+ * A min-heap. 
+ * 
+ * Must contain functionality for the following: (n = # of elements in heap)
+ * 
+ * Adding a new value to the heap  O(log(n))
+ * Popping the front of the heap O(log(n))
+ * Peeking at the front of the heap without removing the element O(1)
+ * Getting the size of the heap O(1)
+ * Checking whether the heap is empty O(1)
+ * 
+ * 
+ * The Heap must hold ints
+ * 
+ * Use List/ArrayList as a backing array, but DO NOT use the 
+ * PriorityQueue class. Do not make Node objects, use array
+ * indexing instead.
+ * 
+ * It is up to you to make design decisions about how to:
+ *  - name methods
+ *  - determine return value and argument types
+ *  - hold private instance variables
+ */
+public class Heap {
+
+}
